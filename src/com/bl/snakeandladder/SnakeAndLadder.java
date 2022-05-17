2463884dec666b01e91641fe547b1934612e44be
nk400 <navkr007@gmail.com>
2022-05-17 18:03:31 +0530

    ADDED: Rollig die to get number

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game.");
         int playerStartPosition = 0;
+        int numberAfterRolling = (int) Math.floor(Math.random() * 10) % 6 +1;
+        System.out.println("Number after rolling the dice by the player: " + numberAfterRolling);
     }
 }
