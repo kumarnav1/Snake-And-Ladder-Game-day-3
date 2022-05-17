1657c46eb6c1e76cc89ea200eb52d1033accd322
nk400 <navkr007@gmail.com>
2022-05-17 23:05:33 +0530

    ADDED: Plyaing untill exact winning position

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -13,12 +13,14 @@ public class SnakeAndLadder {
         while (playerStartPosition < WINNING_POSITION) {
             int numberAfterRolling = (int) Math.floor(Math.random() * 10) % 6 + 1;
             System.out.println("Number after rolling the dice by the player: " + numberAfterRolling);
-            int checkOptions = (int) Math.floor(Math.random() * 10) % 3 ;
+            int checkOptions = (int) Math.floor(Math.random() * 10) % 3;
             switch (checkOptions) {
                 case NO_PLAY:
                     System.out.println("Player is not playing therefore stays in the same position");
                     break;
                 case LADDER:
+                    if (playerStartPosition + numberAfterRolling > 100)
+                        break;
                     playerStartPosition += numberAfterRolling;
                     System.out.println("Player got the ladder and moves ahead then position is: " + playerStartPosition);
                     break;
