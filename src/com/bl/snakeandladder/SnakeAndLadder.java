ac2e5dc1e053f616770a7448aec138d4ee9a4156
nk400 <navkr007@gmail.com>
2022-05-17 17:43:30 +0530

    ADDED: Player's start position added

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,6 @@ package com.bl.snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game.");
+        int playerStartPosition = 0;
     }
 }
