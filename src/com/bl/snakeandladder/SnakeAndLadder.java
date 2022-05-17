b2d547f0205ed307e462da9f5e0e0dc83e77e0ea
nk400 <navkr007@gmail.com>
2022-05-17 18:46:29 +0530

    ADDED: Checking player options

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -1,10 +1,28 @@
 package com.bl.snakeandladder;
 
 public class SnakeAndLadder {
+
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game.");
         int playerStartPosition = 0;
-        int numberAfterRolling = (int) Math.floor(Math.random() * 10) % 6 +1;
+        int numberAfterRolling = (int) Math.floor(Math.random() * 10) % 6 + 1;
         System.out.println("Number after rolling the dice by the player: " + numberAfterRolling);
+        int checkOptions = (int) Math.floor(Math.random() * 10) % 3;
+        switch (checkOptions) {
+            case NO_PLAY:
+                System.out.println("Player is not playing therefore stays in the same position");
+                break;
+            case LADDER:
+                playerStartPosition += numberAfterRolling;
+                System.out.println("Player got the ladder and moves ahead then position is: " + playerStartPosition);
+                break;
+            case SNAKE:
+                playerStartPosition -= numberAfterRolling;
+                System.out.println("Player got snake bite and got behind then position is: " + playerStartPosition);
+        }
     }
 }
